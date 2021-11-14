340fa683b9211e1ba786d056092bfc2a10ebfa68
roandr1970 <ambal007a@yandex.ru>
2021-11-14 16:35:10 +0300

    corrected DataGenerator and AuthTest

---
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -0,0 +1,30 @@
+package ru.netology.testmode.test;
+
+import lombok.var;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
+import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
+import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
+public class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[type='text']").setValue(registeredUser.getLogin());
+        $("[type='password']").setValue(registeredUser.getPassword());
+        $(withText("Продолжить")).click();
+    }
+}
